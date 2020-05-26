29db9d2f06bfa1f122b5e83f4263c0a20137d33b
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2020-05-26 22:15:12 +0800

    Add background colour and styling

--- scripts/map.js
+++ scripts/map.js
@@ -1,30 +1,24 @@
 const map_src = "map_data/singapore-map-simplified.json";
+let map_data;
+let map_feature;
 
-let map_container = document.querySelector("#map-container");
+function init_map() {
+    let map_container = document.querySelector("#map-container");
 
-let map_width = map_container.offsetWidth,
-    map_height = map_container.offsetHeight;
+    let map_width = map_container.offsetWidth,
+        map_height = map_container.offsetHeight;
 
-let map_svg = d3.select("#map-container")
-    .append("svg")
-    .attr("width", map_width)
-    .attr("height", map_height);
+    let map_svg = d3.select("#map-container")
+        .append("svg")
+        .attr("width", map_width)
+        .attr("height", map_height);
 
-let g = map_svg.append("g");
+    let g = map_svg.append("g");
 
-let projection = d3.geoEquirectangular()
-    .scale(80000)
-    .center([103.8536, 1.2789])
-    .translate([map_width/2, map_height/2]);
-
-let map;
-
-d3.json(map_src).then((map_data) => {
-    let map_feature = topojson.feature(
-        map_data,
-        map_data.objects["sla-cadastral-land-parcel-geojson"]);
-
-    projection.fitExtent([[0, 0], [map_width, map_height]], map_feature);
+    let projection = d3.geoEquirectangular()
+        .center([103.8536, 1.2789])
+        .translate([map_width/2, map_height/2])
+        .fitExtent([[0, 0], [map_width, map_height]], map_feature);
 
     let path = d3.geoPath()
         .projection(projection);
@@ -33,5 +27,14 @@ d3.json(map_src).then((map_data) => {
         .enter()
         .append("path")
         .attr("fill", "grey")
-        .attr("d", path);
+        .attr("d", path)
+        .attr("class", "map");
+}
+
+d3.json(map_src).then((data) => {
+    map_data = data;
+    map_feature = topojson.feature(
+        map_data,
+        map_data.objects["sla-cadastral-land-parcel-geojson"]);
+    init_map();
 });
